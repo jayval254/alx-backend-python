75a28019d54b19dc71a01f11812a9b850fb7a7e8
jayval254 <valariekimuge1@gmail.com>
2024-06-06 17:43:58 +0000

    Python variable annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function to_str that takes a float n as argument and returns the string representation of the float"""
+
+
+def to_str(n: float) -> str:
+    """Returns a string representation of a float
+    Args:
+        n: A float to convert to a string
+    Returns:
+        A string representation of the float
+    """
+    return str(n)
